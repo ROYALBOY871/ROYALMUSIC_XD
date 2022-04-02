63d0e1b428470201fadd548ddc2c5f9bcdac4b66
ROYALBOY <98400258+ROYALBOY871@users.noreply.github.com>
2022-04-02 20:47:15 +0530

    Update miscs.py

--- royalmusic/miscs.py
+++ royalmusic/miscs.py
@@ -1,6 +1,6 @@
 HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
-**/start** __To Start The bot.__
-**/help** __To Show This Message.__
+**/start** __for starting the bot.__
+**/help** __To show help menu.__
 **/ping** __To Show Latency Bot.__
 **/skip** __To Skip The Current Playing Music.__
 **/play** __youtube/saavn/deezer song_name__
@@ -14,15 +14,15 @@ HELP_TEXT = """__**I Can Play Music In The Voice Chat**__
 **/repo** __To Source Code This Not.__
 **/donation** __To Give Me A Coffe.__"""
 
-START_TEXT = "__**Hi I'm KryPtoN Music Bot. I will sing for you on Voice Call Telegram, Hit /help for look my features.**__"
+START_TEXT = "__**Hii! I am ROYALMUSIC_XD an music bot.... For any help type /help or contact to [👨‍💻 𝙇𝙀𝙂𝙀𝙉𝘿 👨‍💻](t.me/KartiK_KinG01)**__"
 
 REPO_TEXT = """Source code this bot 🤖
-Source code: [Github](https://github.com/Kry9toN/KryPtoN-Music-Bot)
-License: [ GPL-3.0 License](https://github.com/Kry9toN/KryPtoN-Music-Bot/blob/master/LICENSE.md)"""
+Source code: [Github](https://github.com/ROYALBOY871/ROYALMUSIC_XD)
+License: [ GPL-3.0 License](https://github.com/ROYALBOY871/ROYALMUSIC_XD/blob/master/LICENSE.md)"""
 
 DONATION_TEXT = """♥️Donation List♥️
-Indonesia:
-[Saweria](https://saweria.co/donate/Kry9toN)
+India:
+[Paytm](https://)
 Internasional:
-[Paypal](https://www.paypal.me/KomodoOS)
+[Paypal](https://www.paypal.me/)
 """
